a83d4f329e2dce1605e05e10a4483fb15a663f68
Matija Novosel <mnovosel5@gmail.com>
2022-10-12 19:59:02 +0200

    Items wip

--- pages/items.vue
+++ pages/items.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="offset w-full">
-    <div class="text-4xl font-bold mb-5 mt-6 flex items-center justify-center">
+    <div class="text-4xl font-bold mb-5 mt-6 flex items-center justify-center user-select-none">
       <div @click="activeTab = Tabs.UT" class="ripple px-3 py-1 cursor-pointer rounded-lg" :class="{
         'text-green-vue': activeTab === Tabs.UT
       }">
@@ -16,9 +16,9 @@
       </div>
     </div>
     <div class=" grid grid-cols-12 gap-4 justify-center">
-      <div class="item bg-dark-700 rounded-lg p-3 col-span-1" v-for="n in 100" :key="n">
-        Item
-      </div>
+      <div class="item bg-dark-700 rounded-lg p-3 col-span-1 text-center relative" v-for="n in 100" :key="n" :style="{
+      background: 'url(renders.png)',
+      backgroundPosition: '-13708px -184px'}" />
     </div>
   </div>
 </template>
@@ -34,3 +34,13 @@ const activeTab = ref(Tabs.UT);
 const { setMeta } = useMetadata();
 setMeta("Realm trove | Items");
 </script>
+
+<style>
+.item {
+  display: inline-block;
+  position: relative;
+  width: 46px;
+  margin: -1px -3px -3px;
+  background-image: url("renders.png");
+}
+</style>
