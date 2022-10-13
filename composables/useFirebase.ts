e4f62aaa7a27c9d89dadab77863d6e45b62e165d
Matija Novosel <mnovosel5@gmail.com>
2022-10-14 01:37:15 +0200

    Firebase env changes

--- composables/useFirebase.ts
+++ composables/useFirebase.ts
@@ -2,7 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 export const useFirebase = () => {
-  const firebaseApp = initializeApp(JSON.parse(process.env.firebaseConfig));
+  const firebaseApp = initializeApp({
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId,
+    appId: process.env.appId,
+    measurementId: process.env.measurementId
+  });
+
   const firestore = getFirestore(firebaseApp);
 
   return {
