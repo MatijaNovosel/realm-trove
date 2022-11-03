4314a185ca266857c6b24bd3b1899a74671e7a62
Matija Novosel <mnovosel5@gmail.com>
2022-11-03 15:58:37 +0100

    Marks CRUD

--- pages/quests.vue
+++ pages/quests.vue
@@ -1,62 +1,72 @@
 <template>
-  <div
-    class="offset w-full px-3"
-    :class="{
-      'h-full flex-center': pending || error
-    }"
-  >
-    <spinner class="mt-4 mx-auto" v-if="pending" />
-    <div v-else>
-      <h1>Quests</h1>
-      <div class="row my-5 md:px-0 items-center">
-        <div
-          class="col-span-12 md:col-span-3 flex items-center justify-center md:justify-start user-select-none"
-        >
-          <items-instructions />
-        </div>
-        <div class="col-span-12 md:col-span-6 mt-4 md:mt-0 flex-center">
-          <items-custom-input
-            class="w-full"
-            placeholder="Search marks"
-            dense
-            :loading="pending"
-            :error="error"
-            v-model="state.searchText"
-          />
-          <app-icon-button
-            class="ml-2"
-            tooltip="Reset"
-            icon-color="green-vue"
-            background-color="dark"
+  <div class="contents">
+    <app-confirmation-dialog
+      :open="state.confirmDialogOpen"
+      @close="state.confirmDialogOpen = false"
+      @yes="resetCollection"
+      @no="state.confirmDialogOpen = false"
+    />
+    <div
+      class="offset w-full px-3"
+      :class="{
+        'h-full flex-center': pending || error
+      }"
+    >
+      <spinner class="mt-4 mx-auto" v-if="pending" />
+      <div v-else>
+        <h1>Quests</h1>
+        <div class="row my-5 md:px-0 items-center">
+          <div
+            class="col-span-12 md:col-span-3 flex items-center justify-center md:justify-start user-select-none"
           >
-            <ResetIcon />
-          </app-icon-button>
+            <items-instructions />
+          </div>
+          <div class="col-span-12 md:col-span-6 mt-4 md:mt-0 flex-center">
+            <items-custom-input
+              class="w-full"
+              placeholder="Search marks"
+              dense
+              :loading="pending"
+              :error="error"
+              v-model="state.searchText"
+            />
+            <app-icon-button
+              class="ml-2"
+              tooltip="Reset"
+              icon-color="green-vue"
+              background-color="dark"
+              @on-click="state.confirmDialogOpen = true"
+            >
+              <ResetIcon />
+            </app-icon-button>
+          </div>
+          <div
+            class="col-span-12 md:col-span-3 flex md:justify-end fixed md:relative z-5 right-10 md:right-0 bottom-5 md:bottom-0 h-min"
+          >
+            <app-text-button
+              background-color="error"
+              text="Cancel"
+              class="ml-2"
+              :disabled="!pendingChanges"
+              @on-click="cancelChanges"
+            />
+            <app-text-button
+              background-color="green-vue"
+              text="Save"
+              class="ml-2"
+              :disabled="!pendingChanges"
+              @on-click="confirmChanges"
+            />
+          </div>
         </div>
-        <div
-          class="col-span-12 md:col-span-3 flex md:justify-end fixed md:relative z-5 right-10 md:right-0 bottom-5 md:bottom-0 h-min"
-        >
-          <app-text-button
-            background-color="error"
-            text="Cancel"
-            class="ml-2"
-            :disabled="!pendingChanges"
-            @on-click="cancelChanges"
-          />
-          <app-text-button
-            background-color="green-vue"
-            text="Save"
-            class="ml-2"
-            :disabled="!pendingChanges"
-          />
-        </div>
+        <quests-grid
+          :marks="state.filteredMarks"
+          :collection="data.quests.marks"
+          :initial="state.initialCollection"
+          @increment="updateCollection"
+          @decrement="updateCollection($event, false)"
+        />
       </div>
-      <quests-grid
-        :marks="state.filteredMarks"
-        :collection="data.quests.marks"
-        :initial="state.initialCollection"
-        @increment="updateCollection"
-        @decrement="updateCollection($event, false)"
-      />
     </div>
   </div>
 </template>
@@ -66,8 +76,16 @@ import { useDebounceFn } from "@vueuse/core";
 import { MarkInfo, Profile } from "~/models";
 import { MARKS } from "~/utils/marks";
 import ResetIcon from "~icons/carbon/reset";
+import {
+  doc,
+  DocumentData,
+  DocumentReference,
+  setDoc
+} from "firebase/firestore";
 
 const userData = useUserData();
+const { setMeta } = useMetadata();
+const { $firebaseFirestore } = useNuxtApp();
 const { createToast, removePermanentToasts, permanentToastExists } = useToast();
 
 const { data, pending, error } = await useLazyFetch<Profile>(
@@ -80,8 +98,11 @@ const { data, pending, error } = await useLazyFetch<Profile>(
 const state = reactive({
   initialCollection: undefined,
   filteredMarks: MARKS,
-  searchText: ""
+  searchText: "",
+  confirmDialogOpen: false
 });
+
+const docRef = ref<DocumentReference<DocumentData>>();
 
 const pendingChanges = computed(() => {
   return (
@@ -140,6 +161,33 @@ const searchMarks = useDebounceFn(() => {
   );
 }, 300);
 
+const confirmChanges = async () => {
+  try {
+    await setDoc(docRef.value, data.value);
+    createToast("Saved!", "green-vue");
+    searchMarks();
+    state.initialCollection = undefined;
+  } catch (e) {
+    createToast(e.message, "error");
+    error.value = true;
+  }
+};
+
+const resetCollection = async () => {
+  try {
+    const emptyCollection = { ...data.value };
+    emptyCollection.quests.marks = {};
+    await setDoc(docRef.value, emptyCollection);
+    createToast("Progress has been reset!", "green-vue");
+    state.initialCollection = undefined;
+  } catch (e) {
+    createToast(e.message, "error");
+    error.value = true;
+  } finally {
+    state.confirmDialogOpen = false;
+  }
+};
+
 watch(
   () => [state.searchText],
   () => searchMarks()
@@ -152,8 +200,20 @@ watch(pendingChanges, (val) => {
   }
 });
 
-const { setMeta } = useMetadata();
-setMeta("Realm trove | Quests");
+watch(
+  pending,
+  () => {
+    if (data.value) {
+      docRef.value = doc($firebaseFirestore, "profile", userData.value.shortId);
+      setMeta("Realm trove | Quests");
+    } else {
+      setMeta("Loading ...");
+    }
+  },
+  {
+    immediate: true
+  }
+);
 </script>
 
 <style scoped>
--- utils/marks.ts
+++ utils/marks.ts
@@ -80,5 +80,245 @@ export const MARKS: MarkInfo[] = [
       x: -414,
       y: -138
     }
+  },
+  {
+    id: 11,
+    name: "Mark of Thessal",
+    pos: {
+      x: -460,
+      y: -138
+    }
+  },
+  {
+    id: 12,
+    name: "Mark of Geb",
+    pos: {
+      x: -506,
+      y: -138
+    }
+  },
+  {
+    id: 13,
+    name: "Mark of Oryx",
+    pos: {
+      x: -552,
+      y: -138
+    }
+  },
+  {
+    id: 14,
+    name: "Mark of Janus",
+    pos: {
+      x: -598,
+      y: -138
+    }
+  },
+  {
+    id: 15,
+    name: "Mark of the Wyvern",
+    pos: {
+      x: -644,
+      y: -138
+    }
+  },
+  {
+    id: 16,
+    name: "Mark of the Forgotten King",
+    pos: {
+      x: -690,
+      y: -138
+    }
+  },
+  {
+    id: 17,
+    name: "Mark of Shaitan",
+    pos: {
+      x: -736,
+      y: -138
+    }
+  },
+  {
+    id: 18,
+    name: "Mark of Bilgewater",
+    pos: {
+      x: -782,
+      y: -138
+    }
+  },
+  {
+    id: 19,
+    name: "Mark of the Megamoth",
+    pos: {
+      x: -828,
+      y: -138
+    }
+  },
+  {
+    id: 20,
+    name: "Mark of the Killer Bee Queen",
+    pos: {
+      x: -874,
+      y: -138
+    }
+  },
+  {
+    id: 21,
+    name: "Mark of the Son of Arachna",
+    pos: {
+      x: -920,
+      y: -138
+    }
+  },
+  {
+    id: 22,
+    name: "Mark of Stheno",
+    pos: {
+      x: -966,
+      y: -138
+    }
+  },
+  {
+    id: 23,
+    name: "Mark of Davy Jones",
+    pos: {
+      x: -1012,
+      y: -138
+    }
+  },
+  {
+    id: 24,
+    name: "Mark of Parasitic Horrors",
+    pos: {
+      x: -1058,
+      y: -138
+    }
+  },
+  {
+    id: 25,
+    name: "Mark of the Fountain Spirit",
+    pos: {
+      x: -1104,
+      y: -138
+    }
+  },
+  {
+    id: 26,
+    name: "Mark of the Archivist",
+    pos: {
+      x: -1150,
+      y: -138
+    }
+  },
+  {
+    id: 27,
+    name: "Mark of the Marble Colossus",
+    pos: {
+      x: -1196,
+      y: -138
+    }
+  },
+  {
+    id: 28,
+    name: "Mark of the Void Entity",
+    pos: {
+      x: -1242,
+      y: -138
+    }
+  },
+  {
+    id: 29,
+    name: "Mark of Malus",
+    pos: {
+      x: -1288,
+      y: -138
+    }
+  },
+  {
+    id: 30,
+    name: "Mark of the Effigy",
+    pos: {
+      x: -1334,
+      y: -138
+    }
+  },
+  {
+    id: 31,
+    name: "Mark of Dreadstump",
+    pos: {
+      x: -1380,
+      y: -138
+    }
+  },
+  {
+    id: 32,
+    name: "Mark of Mama Megamoth",
+    pos: {
+      x: -1426,
+      y: -138
+    }
+  },
+  {
+    id: 33,
+    name: "Mark of Arachna",
+    pos: {
+      x: -1472,
+      y: -138
+    }
+  },
+  {
+    id: 34,
+    name: "Mark of the Queen Bee",
+    pos: {
+      x: -1518,
+      y: -138
+    }
+  },
+  {
+    id: 35,
+    name: "Mark of Mixcoatl",
+    pos: {
+      x: -1564,
+      y: -138
+    }
+  },
+  {
+    id: 36,
+    name: "Mark of the Tesseract Goddess",
+    pos: {
+      x: -1610,
+      y: -138
+    }
+  },
+  {
+    id: 37,
+    name: "Mark of the Exalted God",
+    pos: {
+      x: -1656,
+      y: -138
+    }
+  },
+  {
+    id: 38,
+    name: "Mark of the Crystal Entity",
+    pos: {
+      x: -1702,
+      y: -138
+    }
+  },
+  {
+    id: 39,
+    name: "Mark of the Sandstone Titan",
+    pos: {
+      x: -1748,
+      y: -138
+    }
+  },
+  {
+    id: 40,
+    name: "Mark of the Interregnum",
+    pos: {
+      x: -1794,
+      y: -138
+    }
   }
 ];
